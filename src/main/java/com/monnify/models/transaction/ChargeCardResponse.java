71f1af0c0ed6471a5b065802755d0ab6273fbef2
oresho <somuyiwaoreoluwa@gmail.com>
2025-03-12 09:21:48 +0100

    feat: secure 3d data

--- src/main/java/com/monnify/models/transaction/ChargeCardResponse.java
+++ src/main/java/com/monnify/models/transaction/ChargeCardResponse.java
@@ -1,6 +1,8 @@
 package com.monnify.models.transaction;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
@@ -11,4 +13,32 @@ public class ChargeCardResponse {
     private String transactionReference;
     private String status;
     private String message;
+    private OtpData otpData;
+    private Secure3dData secure3dData;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OtpData {
+        private String id;
+        private String message;
+        private String authData;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Secure3dData {
+        private String id;
+        private String redirectURL;
+        private String callBackURL;
+        private String eciFlag;
+        private String termURL;
+        private String acsURL;
+        private String transactionID;
+        private String paymentID;
+        private String method;
+        private String jwt;
+        private String md;
+    }
 }
